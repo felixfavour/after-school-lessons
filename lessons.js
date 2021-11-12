f8972c26ce733a3116526996e8b80119956951e5
Favour Felix <felixfavour0@gmail.com>
2021-11-13 01:36:21 +0400

    added lessons.js file with json

---
+++ lessons.js
@@ -0,0 +1,82 @@
+const lessons = [
+  {
+    subject: 'Swimming',
+    location: 'Lagos, Nigeria',
+    spaces: 5,
+    price: 500,
+    stars: 4,
+    image: '/images/swimming'
+  },
+  {
+    subject: 'Painting',
+    location: 'Abuja, Nigeria',
+    spaces: 5,
+    price: 350,
+    stars: 4,
+    image: '/images/painting'
+  },
+  {
+    subject: 'Football',
+    location: 'London, United Kingdom',
+    spaces: 5,
+    price: 750,
+    stars: 5,
+    image: '/images/football'
+  },
+  {
+    subject: 'Basketball',
+    location: 'Los Angeles, United States',
+    spaces: 5,
+    price: 600,
+    stars: 4,
+    image: '/images/basketball'
+  },
+  {
+    subject: 'Karate',
+    location: 'Abu Dhabi, UAE',
+    spaces: 5,
+    price: 250,
+    stars: 3,
+    image: '/images/karate'
+  },
+  {
+    subject: 'Surfing',
+    location: 'Dubai, UAE',
+    spaces: 5,
+    price: 450,
+    stars: 2,
+    image: '/images/surfing'
+  },
+  {
+    subject: 'Chess',
+    location: 'Lagos, Nigeria',
+    spaces: 5,
+    price: 150,
+    stars: 3,
+    image: '/images/chess'
+  },
+  {
+    subject: 'Scrabble',
+    location: 'Abuja, Nigeria',
+    spaces: 5,
+    price: 50,
+    stars: 4,
+    image: '/images/scrabble'
+  },
+  {
+    subject: 'Dancing',
+    location: 'New York, United States',
+    spaces: 5,
+    price: 150,
+    stars: 5,
+    image: '/images/dancing'
+  },
+  {
+    subject: 'Cooking',
+    location: 'Lagos, Nigeria',
+    spaces: 5,
+    price: 200,
+    stars: 3,
+    image: '/images/cooking'
+  }
+]
